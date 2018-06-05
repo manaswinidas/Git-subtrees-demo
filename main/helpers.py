574074920999f0271f7269feabfae80a68bece4b
Manaswini Das <dasmanaswini10@gmail.com>
2018-06-06 01:03:40 +0530

    Changes env.example, main/helpers.py and urls.py

--- main/helpers.py
+++ main/helpers.py
@@ -4,14 +4,14 @@ import arrow
 from datetime import timedelta
 
 
-def get_datasource_file(oh_member):
+def get_github_file(oh_member):
     try:
         oh_access_token = oh_member.get_access_token(
                                 client_id=settings.OPENHUMANS_CLIENT_ID,
                                 client_secret=settings.OPENHUMANS_CLIENT_SECRET)
         user_object = api.exchange_oauth2_member(oh_access_token)
         for dfile in user_object['data']:
-            if 'demo' in dfile['metadata']['tags']:
+            if 'Github' in dfile['metadata']['tags']:
                 return dfile['download_url']
         return ''
 
@@ -19,7 +19,7 @@ def get_datasource_file(oh_member):
         return 'error'
 
 
-def check_update(datasource_member):
-    if datasource_member.last_submitted < (arrow.now() - timedelta(hours=1)):
+def check_update(github_member):
+    if github_member.last_submitted < (arrow.now() - timedelta(hours=1)):
         return True
     return False
--- main/urls.py
+++ main/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('complete/', views.complete, name='complete'),
-    path('datasource_complete/', views.datasource_complete, name='datasource_complete'),
+    path('github_complete/', views.github_complete, name='github_complete'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('update_data/', views.update_data, name='update_data'),
-    path('remove_datasource/', views.remove_datasource, name='remove_datasource')
+    path('remove_github/', views.remove_github, name='remove_github')
 ]
